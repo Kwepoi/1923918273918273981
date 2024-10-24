70c26166214bf35199a27baef20ecfc0001b9ab4
Kwepoi <79616784+Kwepoi@users.noreply.github.com>
2024-10-24 11:50:05 +0300

    Add files via upload

---
+++ user-overrides.js
@@ -0,0 +1,29 @@
+// Disable Pocket core functionality
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.pocket.api", "");
+user_pref("extensions.pocket.site", "");
+user_pref("extensions.pocket.oAuthConsumerKey", "");
+user_pref("extensions.pocket.oAuthConsumerKeyBff", "");
+user_pref("extensions.pocket.bffApi", "");
+
+// Disable Pocket in new tab page and discovery stream
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.saveToPocketCard.enabled", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.pocket-feed-parameters", "");
+user_pref("browser.newtabpage.activity-stream.pocketCta", "");
+
+// Disable Pocket in URL bar
+user_pref("browser.urlbar.pocket.featureGate", false);
+user_pref("browser.urlbar.suggest.pocket", false);
+
+// Disable additional Pocket features
+user_pref("extensions.pocket.bffRecentSaves", false);
+user_pref("extensions.pocket.showHome", false);
+user_pref("extensions.pocket.refresh.emailButton.enabled", false);
+user_pref("extensions.pocket.refresh.hideRecentSaves.enabled", false);
+user_pref("extensions.pocket.loggedOutVariant", "");
+
+// Disable Pocket sync settings
+user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("services.sync.prefs.sync-seen.browser.newtabpage.activity-stream.section.highlights.includePocket", false);

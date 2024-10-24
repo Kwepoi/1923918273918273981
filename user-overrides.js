a611d5cf7ac98a2e218255c52bb484d0521f6275
Kwepoi <79616784+Kwepoi@users.noreply.github.com>
2024-10-24 19:35:39 +0300

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -27,3 +27,11 @@ user_pref("extensions.pocket.loggedOutVariant", "");
 // Disable Pocket sync settings
 user_pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket", false);
 user_pref("services.sync.prefs.sync-seen.browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+
+// Disable Firefox View
+user_pref("browser.tabs.firefox-view", false);
+user_pref("browser.firefox-view.view-count", 0);
+user_pref("browser.tabs.firefox-view.notify-for-tabs", false);
+user_pref("browser.firefox-view.feature-tour", "");
+user_pref("browser.tabs.firefox-view.logLevel", "Off");
+user_pref("services.sync.prefs.sync.browser.firefox-view.feature-tour", false);
